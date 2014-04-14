b8f7a9d1bdc12005d78f205e1fda0eef5582db4f
angelbirth <ricky.ason@gmail.com>
2014-04-14 19:00:31 +0700

    added traversal methods

--- Tree/src/org/ric/tree/Tree.java
+++ Tree/src/org/ric/tree/Tree.java
@@ -1,50 +1,53 @@
 package org.ric.tree;
 
 import java.io.Serializable;
+import java.util.function.Consumer;
 
 public class Tree<E extends Comparable<E>> implements Serializable {
-    public static class TreeNode<E extends Comparable<E>> {
-        private E data;
-        private TreeNode<E> left, right, parent;
+    public static final int PREORDER = 0, INORDER = 1, POSTORDER = 2;
 
-        public TreeNode(E data, TreeNode<E> parent) {
+    public static class TreeNode<T extends Comparable<T>> {
+        private T data;
+        private TreeNode<T> left, right, parent;
+
+        public TreeNode(T data, TreeNode<T> parent) {
             this.data = data;
             this.parent = parent;
         }
 
-        public TreeNode(E data) {
+        public TreeNode(T data) {
             this(data, null);
         }
 
-        public E getData() {
+        public T getData() {
             return data;
         }
 
-        public void setData(E data) {
+        public void setData(T data) {
             this.data = data;
         }
 
-        public TreeNode<E> getLeft() {
+        public TreeNode<T> getLeft() {
             return left;
         }
 
-        void setLeft(TreeNode<E> left) {
+        void setLeft(TreeNode<T> left) {
             this.left = left;
         }
 
-        public TreeNode<E> getRight() {
+        public TreeNode<T> getRight() {
             return right;
         }
 
-        void setRight(TreeNode<E> right) {
+        void setRight(TreeNode<T> right) {
             this.right = right;
         }
 
-        public TreeNode<E> getParent() {
+        public TreeNode<T> getParent() {
             return parent;
         }
 
-        void setParent(TreeNode<E> parent) {
+        void setParent(TreeNode<T> parent) {
             this.parent = parent;
         }
     }
@@ -81,5 +84,52 @@ public class Tree<E extends Comparable<E>> implements Serializable {
         } else {
             insert(data, root);
         }
+    }
+
+    private void preOrder(TreeNode<E> localRoot, Consumer<E> action) {
+        if (localRoot == null) {
+            return;
+        }
+        action.accept(localRoot.data);
+        preOrder(localRoot.left, action);
+        preOrder(localRoot.right, action);
+    }
+
+    private void inOrder(TreeNode<E> localRoot, Consumer<E> action) {
+        if (localRoot == null) {
+            return;
+        }
+        inOrder(localRoot.left, action);
+        action.accept(localRoot.data);
+        inOrder(localRoot.right, action);
+    }
+
+    private void postOrder(TreeNode<E> localRoot, Consumer<E> action) {
+        if (localRoot == null) {
+            return;
+        }
+        postOrder(localRoot.left, action);
+        postOrder(localRoot.right, action);
+        action.accept(localRoot.data);
+    }
+
+    public void traverse(int order, Consumer<E> action) {
+        switch (order) {
+        case PREORDER:
+            preOrder(root, action);
+            break;
+        case INORDER:
+            inOrder(root, action);
+            break;
+        case POSTORDER:
+            postOrder(root, action);
+            break;
+        default:
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void traverse(int order) {
+        traverse(order, x -> System.out.print(x + " "));
     }
 }
