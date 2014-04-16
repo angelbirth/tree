998463a8942616a430f35aa6a13fc38595abb6ff
angelbirth <ricky.ason@gmail.com>
2014-04-16 15:18:15 +0700

    added several methods - TreeNode.childCount() - TreeNode.isLeftChild() - TreeNode.isRightChild() - TreeNode.equals() - Tree.successor() - Tree.remove01() - Tree.remove2()
    documentations will follow

--- Tree/src/org/ric/tree/Tree.java
+++ Tree/src/org/ric/tree/Tree.java
@@ -1,6 +1,7 @@
 package org.ric.tree;
 
 import java.io.Serializable;
+import java.util.TreeMap;
 import java.util.function.Consumer;
 
 public class Tree<E extends Comparable<E>> implements Serializable {
@@ -13,6 +14,29 @@ public class Tree<E extends Comparable<E>> implements Serializable {
         public TreeNode(T data, TreeNode<T> parent) {
             this.data = data;
             this.parent = parent;
+        }
+
+        public boolean equals(TreeNode<T> n) {
+            if (n == null)
+                return false;
+            if (!data.equals(n.data) && data.compareTo(n.data) != 0)
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            // TODO Auto-generated method stub
+            return data.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null)
+                return false;
+            if (!(obj instanceof TreeNode<?>))
+                return false;
+            return equals((TreeNode<T>) obj);
         }
 
         public TreeNode(T data) {
@@ -71,6 +95,26 @@ public class Tree<E extends Comparable<E>> implements Serializable {
                     return Math.max(left.height(), right.height()) + 1;
                 }
             }
+        }
+
+        private boolean isLeftChild() {
+            if (isRoot())
+                return false;
+            return parent.left == this;
+        }
+
+        private boolean isRightChild() {
+            if (isRoot())
+                return false;
+            return parent.right == this;
+        }
+
+        int childCount() {
+            if (isLeaf())
+                return 0;
+            if (left != null && right != null)
+                return 2;
+            return 1;
         }
     }
 
@@ -153,9 +197,10 @@ public class Tree<E extends Comparable<E>> implements Serializable {
 
     public void traverse(int order) {
         traverse(order, x -> System.out.print(x + " "));
+        System.out.println();
     }
 
-    public TreeNode<?> find(E data) {
+    public TreeNode<E> find(E data) {
         TreeNode<E> i = root;
         while (i != null) {
             if (data.compareTo(i.data) < 0) {
@@ -168,4 +213,88 @@ public class Tree<E extends Comparable<E>> implements Serializable {
         return null;
     }
 
+    private TreeNode<E> remove01(TreeNode<E> node) {
+        if (node.left != null && node.right != null) {
+            throw new IllegalStateException();
+        }
+        if (node.isRoot()) {
+            if (node.isLeaf()) {
+                root = null;
+            } else {
+                root = node.left != null ? node.left : node.right;
+            }
+        } else {
+            // downward
+            if (node.isLeftChild()) {
+                node.parent.left = node.left != null ? node.left : node.right;
+            } else if (node.isRightChild()) {
+                node.parent.right = node.left != null ? node.left : node.right;
+            }
+        }
+        // upward
+        if (node.left != null) {
+            node.left.parent = node.parent;
+        } else if (node.right != null) {
+            node.right.parent = node.parent;
+        }
+        node.left = node.right = node.parent = null;
+        return node;
+    }
+
+    private TreeNode<E> remove2(TreeNode<E> node) {
+        TreeNode<E> a = successor(node);
+        TreeNode<E> successor = remove01(a);
+        successor.left = node.left;
+        successor.right = node.right;
+        successor.parent = node.parent;
+        if (node.isRoot()) {
+            root = successor;
+        } else {
+            // downward
+            if (node.isLeftChild()) {
+                node.parent.left = successor;
+            } else if (node.isRightChild()) {
+                node.parent.right = successor;
+            }
+        }
+        // upward
+        if (node.left != null)
+            node.left.parent = successor;
+        if (node.right != null)
+            node.right.parent = successor;
+        node.left = node.right = node.parent = null;
+        return node;
+    }
+
+    public TreeNode<E> remove(E data) {
+        TreeNode<E> del = find(data);
+        if (del == null)
+            return null;
+        switch (del.childCount()) {
+        case 0:
+        case 1:
+            return remove01(del);
+        default:
+            return remove2(del);
+        }
+    }
+
+    static <E extends Comparable<E>> TreeNode<E> successor(TreeNode<E> t) {
+        if (t == null)
+            return null;
+        else if (t.right != null) {
+            TreeNode<E> p = t.right;
+            while (p.left != null)
+                p = p.left;
+            return p;
+        } else {
+            TreeNode<E> p = t.parent;
+            TreeNode<E> ch = t;
+            while (p != null && ch == p.right) {
+                ch = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
 }
